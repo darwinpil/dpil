a28906b78e81c4035a87aa6962c84d29116cd644
darwinpil <104120688+darwinpil@users.noreply.github.com>
2022-04-21 13:22:42 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "DPRL" && password == "JAIDARWIN"){
+if ( username == "DPIL" && password == "JAIDARWIN"){
 alert ("Login successfully");
 window.location = "welcome.html"; // Redirecting to other page.
 return false;
